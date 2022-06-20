43b113fc9bb6e107639e12cef61b3e263b229721
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-06-20 17:18:54 +0300

    Update Attributes_values.h

--- Attributes_values.h
+++ Attributes_values.h
@@ -11,10 +11,10 @@ private:
 public:
     Attributes_values();
     Attributes_values(std::string,std::string);
-    Attributes_values(const Attributes_values&);
     std::string get_attribute()const;
     std::string get_value()const;
     void set_value(std::string);
+    void set_attribute(std::string);
     friend std::ostream& operator<<(std::ostream&, Attributes_values&);
 };
 
