2cbf7a1384deb6b6fb5d87e65751977bcdec5231
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-06-20 17:19:30 +0300

    Update Attributes_values.cpp

--- Attributes_values.cpp
+++ Attributes_values.cpp
@@ -6,12 +6,6 @@ Attributes_values::Attributes_values()
 Attributes_values::Attributes_values(std::string _attribute, std::string _value)
 :attribute(_attribute), value(_value){}
 
-Attributes_values::Attributes_values(const Attributes_values& other)
-{
-    attribute=other.attribute;
-    value=other.value;
-}
-
 std::string Attributes_values::get_attribute()const
 {
     return attribute;
@@ -30,4 +24,8 @@ std::ostream& operator<<(std::ostream& os, Attributes_values& other)
  void Attributes_values::set_value(std::string _value)
  {
      value=_value;
+ }
+ void Attributes_values::set_attribute(std::string _attribute)
+ {
+     attribute=_attribute;
  }
